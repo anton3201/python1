f3331dffd7428c260f2ec3fc363c3e9609388361
Anton Arkhipov <anton3201@icloud.com>
2024-06-16 15:16:42 +0300

    Second

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import requests
+import json
+from flask import Flask
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def get_valutes_list():
+    url = 'https://www.cbr-xml-daily.ru/daily_json.js'
+    response = requests.get(url)
+    data = json.loads(response.text)
+    valutes = list(data['Valute'].values())
+    return valutes
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+app = Flask(__name__)
+
+
+def create_html(valutes):
+    text = '<h1>Курс валют</h1>'
+    text += '<table>'
+    text += '<tr>'
+    for _ in valutes[0]:
+        text += f'<th><th>'
+    text += '</tr>'
+    for valute in valutes:
+        text += '<tr>'
+        for v in valute.values():
+            text += f'<td>{v}</td>'
+        text += '</tr>'
+
+    text += '</table>'
+    return text
+
+
+@app.route("/")
+def index():
+    valutes = get_valutes_list()
+    html = create_html(valutes)
+    return html
+
+
+if __name__ == "__main__":
+    app.run()
